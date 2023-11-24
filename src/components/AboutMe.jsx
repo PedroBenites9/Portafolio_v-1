ade771f1533224c70b062b1a37e70a09d3e6223a
Pedro <pedro.benites99@hotmail.com>
2023-11-24 12:28:35 -0300

    Actualizacion

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,3 +1,5 @@
+import { Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
 
 const Section = styled.div`
@@ -10,7 +12,11 @@ const ContenedorAboutMe = styled.div``;
 const AboutMe = () => {
   return (
     <Section>
-      <ContenedorAboutMe></ContenedorAboutMe>
+      <ContenedorAboutMe>
+        <Canvas>
+          <Stage environment={"city"} intensity={0.1}></Stage>
+        </Canvas>
+      </ContenedorAboutMe>
     </Section>
   );
 };
